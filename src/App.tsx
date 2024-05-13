53b79fbd87fadccb37b9278110428c91bd88b764
NIkolas <nikolas@ukr.net>
2024-05-13 21:37:38 +0300

    Change action folder to dist

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import { TonProof } from "./components/TonProof/TonProof";
 export const App = () => (
 
   <TonConnectUIProvider
-  manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
+  manifestUrl="https://hlam.github.io/ton-crypto-match/tonconnect-manifest.json"
   uiPreferences={{ theme: THEME.DARK }}
   actionsConfiguration={{
       twaReturnUrl: 'https://t.me/DemoDappWithTonConnectBot/demo'
