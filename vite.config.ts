750528295d9c7a370f7d0cae1ae4a1f6ec09e7e2
NIkolas <nikolas@ukr.net>
2024-05-13 21:48:07 +0300

    Add BASE

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // @ts-ignore
+  base: process.env.GH_PAGES ? '/ton-crypto-match/' : './',
+  server: {
+    fs: {
+      allow: ['../sdk', './'],
+    },
+  },
 })
