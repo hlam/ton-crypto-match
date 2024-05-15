d33ee6d78651e88bd5a58bbfe41078e645582140
NIkolas <nikolas@ukr.net>
2024-05-15 08:13:36 +0300

    Fix error

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import {App} from './App'
-import {runSingleInstance} from "./utils/run-signle-instance";
+// import {runSingleInstance} from "./utils/run-signle-instance";
 
 eruda.init();
 /*
